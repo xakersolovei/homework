b9934dc2822d0084585325a45ca4fddfd1d2feb9
Sergey A <sergey.abramenka@gmail.com>
2021-09-06 13:21:45 +0300

    homework 3 (str): chess FEN - add extra task
    Add additional task to generate a "human-friendly" printable chess board

--- chess_fen/test_FEN.py
+++ chess_fen/test_FEN.py
@@ -1,4 +1,6 @@
-from FEN import calc_chess_balance
+from FEN import calc_chess_balance, chess_board
+
+# --- diff ---
 
 
 def test_starting_pos():
@@ -19,3 +21,38 @@ def test_karpov_kasparov_1985_24_champ():
 def test_kasparov_karpov_1990_20_queen_sac():
     fen = "r3Rb1k/8/p2p3q/3n1bN1/Pn6/6RP/1p3PPK/1B6 w - - 0 35"
     assert calc_chess_balance(fen) == -9
+
+
+# --- boards ---
+
+
+def test_board_starting_pos():
+    fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    board = """\
+r n b q k b n r
+p p p p p p p p
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+P P P P P P P P
+R N B Q K B N R\
+"""
+
+    assert chess_board(fen) == board
+
+
+def test_board_kasparov_karpov_1985_24_champ():
+    fen = "4r2k/q5bp/4R3/4P1P1/P1Qn3R/2N5/1r4KP/8 w - - 1 43"
+    board = """\
+. . . . r . . k
+q . . . . . b p
+. . . . R . . .
+. . . . P . P .
+P . Q n . . . R
+. . N . . . . .
+. r . . . . K P
+. . . . . . . .\
+"""
+
+    assert chess_board(fen) == board
