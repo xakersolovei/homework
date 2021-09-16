4a4e91b60c237b85f1253e8186d77c5d05073b5a
Aleksey Beynar <xakersolovei@gmail.com>
2021-09-16 23:58:20 +0300

    Complete Atbash, Fen, Password_checker and Str_remove

--- atbash_cipher/atbash.py
+++ atbash_cipher/atbash.py
@@ -13,12 +13,41 @@
 `Bambarbia, Kirgudu` -> `yznyz iyrzp ritfw f`
 """
 
+import string
+
 BLOCK_SIZE = 5
 
+ABC = string.ascii_lowercase
+ATBASH_ABC = ABC[::-1]
+IGNORE = set(string.punctuation + string.whitespace)
 
 def atbash_encode(text: str) -> str:
-    pass
+    encoded = list()
+    for character in text:
+        character = character.lower()
+        index = ABC.find(character)
+        if index < 0:
+            if character not in IGNORE:
+                encoded.append(character)
+        else:
+            encoded.append(ATBASH_ABC[index])
 
+    spaces = 0
+    for p in range(5, len(encoded), 5):
+        encoded.insert(p + spaces, ' ')
+        spaces += 1
+
+    return ''.join(encoded)
 
 def atbash_decode(cipher: str) -> str:
-    pass
+    cipher = cipher.replace(' ', '')
+    decoded = list()
+
+    for character in cipher:
+        index = ATBASH_ABC.find(character)
+        if index < 0:
+            decoded.append(character)
+        else:
+            decoded.append(ABC[index])
+
+    return ''.join(decoded)
